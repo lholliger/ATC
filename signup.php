7e91ac7db4e035d79aba65b5c1ebc22164fad749
Hampton <hampmoe@icloud.com>
2017-05-28 16:24:43 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,9 +4,14 @@ require("header.php");
  <div class='container'>
    <h1>Sign Up</h1>
 
-   <form action="generate.php" method="post">
+   <form action="generate.php" method="post" id="form">
    Username: <input type="text" name="username"><br>
    Password: <input type="password" name="pass0"><br>
    Confirm Password: <input type="password" name="pass1"><br>
    <input type="submit">
    </form>
+  <script>document.body.onkeydown = function(e){
+   if(e.keyCode == 13){
+     document.getElementById("form").submit();
+   }
+};</script>
